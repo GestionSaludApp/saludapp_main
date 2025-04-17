cb93f09244cb483e388a70ee7a06249a3d798ca7
FranciscoRoy <franciscoroy.dm@gmail.com>
2025-04-17 20:26:32 -0300

    ionicAndroid

---
+++ android/app/src/main/java/com/saludapp/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.saludapp.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.saludapp.app',
+  appName: 'saludapp',
+  webDir: 'dist/saludapp/browser'
+};
+
+export default config;
