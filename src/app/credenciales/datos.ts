ca2e16dc798a9f75e0a8af5b76d627fce7e9ed3e
GestionSaludApp <gestionSaludApp@gmail.com>
2025-10-03 15:07:31 -0300

    cred4

--- src/app/credenciales/datos.ts
+++ src/app/credenciales/datos.ts
@@ -5,4 +5,4 @@ export const prefijoImagen = 'https://res.cloudinary.com/' + nombreRepositorioIm
 
 //EN MDBGO: export const bddURL = 'https://saludapp-saludapp.mdbgo.io';
 //EN VERCEL:
-export const bddURL = 'https://saludappbdd.vercel.app/';
+export const bddURL = 'https://saludappbdd.vercel.app';
